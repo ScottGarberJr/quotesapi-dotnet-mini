c6af363bcbc489ff3a8d4d5c3168bbaa5dee7d03
Scott Garber <scottgarberjr1@gmail.com>
2022-07-24 02:50:56 -0400

    added DTO and a 'Secret' property to Quote class for testing

--- QuotesApi/Program.cs
+++ QuotesApi/Program.cs
@@ -4,54 +4,68 @@ using QuotesApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services ((optional)) to the container here before the app definition.
+
 builder.Services.AddDbContext<QuotesDb>(opt => opt.UseInMemoryDatabase("QuoteList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
-// Add Request Mappings here:
-app.MapGet("/", () => "Welcome! This is a basic Quotes API");
-app.MapGet("/quotes/all", async (QuotesDb db) =>
-    await db.Quotes.ToListAsync());
+// =============================================================================
+// Add Requests here:
 
-app.MapGet("/quotes/{id}", async (int id, QuotesDb db) =>
-    await db.Quotes.FindAsync(id)
-        is Quote quote
-            ? Results.Ok(quote)
-            : Results.NotFound());
+app.MapGet("/", () => "Welcome! This is a basic Quotes API");                                                   // Define Index (NOT NEEDED) //
 
-app.MapPost("/quotes", async (Quote quote, QuotesDb db) =>
+app.MapPost("/api/quotes", async (QuoteDTO quoteDTO, QuotesDb db) =>                                            // Create new quote (POST) //
 {
+    var quote = new Quote
+    {
+        Content = quoteDTO.Content,
+        Source = quoteDTO.Source,
+        SubSource = quoteDTO.SubSource
+    };
     db.Quotes.Add(quote);
     await db.SaveChangesAsync();
 
-    return Results.Created($"/quotes/{quote.Id}", quote);
+    return Results.Created($"/quotes/{quote.Id}", new QuoteDTO(quote)); //returns contents of quote created
 });
 
-app.MapPut("/quotes/{id}", async (int id, Quote inputquote, QuotesDb db) =>
+app.MapGet("/api/quotes", async (QuotesDb db) =>                                                                // Read all quotes (GET) //
+    // await db.Quotes.ToListAsync());         //without DTO
+    await db.Quotes.Select(x => new QuoteDTO(x)).ToListAsync());
+
+app.MapGet("/api/quotes/{id}", async (int id, QuotesDb db) =>                                                   // Read a quote by Id (GET) //
+    await db.Quotes.FindAsync(id)
+        is Quote quote
+            ? Results.Ok(new QuoteDTO(quote))     //without DTO is just Results.Ok(quote)
+            : Results.NotFound());
+
+app.MapPut("/api/quotes/{id}", async (int id, QuoteDTO quoteDTO, QuotesDb db) =>                                // Update a quote by Id (PUT) //
 {
-    var quote = await db.Quotes.FindAsync(id);
+    var quote = await db.Quotes.FindAsync(id); //looks in db for quote by Id, then points to it
 
-    if (quote is null) return Results.NotFound();
+    if (quote is null) return Results.NotFound(); //if Id doesnt exist, 404 response
 
-    quote.Content = inputquote.Content;
-    quote.Source = inputquote.Source;
+    quote.Content = quoteDTO.Content;
+    quote.Source = quoteDTO.Source;
+    quote.SubSource = quoteDTO.SubSource;
 
     await db.SaveChangesAsync();
 
-    return Results.NoContent();
+    return Results.NoContent(); 
+    //this Results.NoContent() is the reason it returns a 202 response rather than the content modified like in POST ??
 });
 
-app.MapDelete("/quotes/{id}", async (int id, QuotesDb db) =>
+app.MapDelete("/api/quotes/{id}", async (int id, QuotesDb db) =>                                                // Delete a quote by Id //
 {
-    if (await db.Quotes.FindAsync(id) is Quote quote)
+    if (await db.Quotes.FindAsync(id) is Quote quote) //if it can find and point to the quote by it's id:
     {
-        db.Quotes.Remove(quote);
-        await db.SaveChangesAsync();
-        return Results.Ok(quote);
+        db.Quotes.Remove(quote); //remove it
+        await db.SaveChangesAsync(); //save changes to the db
+        return Results.Ok(new QuoteDTO(quote)); //return only the DTO details of what was deleted
     }
 
-    return Results.NotFound();
+    return Results.NotFound(); //else 404 response
 });
+// =============================================================================
 
 app.Run();
---
+++ QuotesApi/src/Quote.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace QuotesApi.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string? Source { get; set; }
+        public string? SubSource { get; set; }
+        public string? Secret { get; set; }
+        
+    }
+
+    class QuotesDb : DbContext
+    {
+        public QuotesDb(DbContextOptions<QuotesDb> options)
+            : base(options) { }
+
+        public DbSet<Quote> Quotes => Set<Quote>();
+    }
+
+    public class QuoteDTO
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string? Source { get; set; }
+        public string? SubSource { get; set; }
+
+        public QuoteDTO() { }
+        public QuoteDTO(Quote QuoteItem) =>
+        (Id, Content, Source, SubSource) = (QuoteItem.Id, QuoteItem.Content, QuoteItem.Source, QuoteItem.Source);
+    }
+
+}
